5521cfe76714be349f812dda881e5b30e9368799
James Karnes <karnes.james@gmail.com>
2023-04-08 11:05:41 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-image", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "Unknown"
+    }
+  }, "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
