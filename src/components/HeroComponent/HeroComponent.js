c3e55a593115935a9bec11610f840b6996a3b9b5
James Karnes <karnes.james@gmail.com>
2023-04-10 18:15:30 -0400

    started about us page / added engines node to package.json

--- src/components/HeroComponent/HeroComponent.js
+++ src/components/HeroComponent/HeroComponent.js
@@ -5,8 +5,8 @@ const HeroComponent = ({ page, h1, p }) => {
     <div className="absolute top-0 w-full h-[75vh] lg:h-[80vh]">
       <div className={`hero bgImage ${page}`}>
         <div className="hero-text flex flex-col justify-center items-center text-primary-color w-11/12 md:w-9/12 lg:w-6/12 xl:w-4/12">
-          <h1 className="text-4xl md:text-6xl font-bold mb-6">{h1}</h1>
-          <p className="text-lg md:text-2xl font-light xl:w-3/4">{p}</p>
+          {h1 && <h1 className="text-4xl md:text-6xl font-bold mb-6">{h1}</h1>}
+          {p && <p className="text-lg md:text-2xl font-light xl:w-3/4">{p}</p>}
         </div>
       </div>
     </div>
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,13 +1,40 @@
 import React from "react"
 import Layout from "../../components/layout"
 import HeroComponent from "../../components/HeroComponent/HeroComponent"
-
+import TextComponent from "../../components/TextComponent/TextComponent"
+import image from "../../images/tours/horse.jpg"
 const index = () => {
   return (
     <Layout>
       <main>
         <HeroComponent page="indexMid" h1="About Us" />
-        <div className="mt-[85vh]"></div>
+        <div className="mt-[80vh] xl:mt-[85vh]"></div>
+        <div className="max-w-6xl mx-auto space-y-4 p-4 flex flex-col md:flex-row justify-center items-center">
+          <TextComponent
+            title="Title about Us"
+            paragraph="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non consequatur voluptatem deserunt, delectus est deleniti. Illum, incidunt ab at accusamus corporis voluptates qui itaque reiciendis repudiandae doloremque sapiente ducimus iusto et ex magnam. Sed molestias quibusdam dolorum, quos vitae sequi autem perspiciatis aut ab obcaecati fuga eum tempore inventore maxime!"
+            className=""
+          />
+          <div className="w-full flex justify-center items-center mx-5 my-5">
+            <img
+              src={image}
+              alt=""
+              className="h-auto lg:max-w-sm xl:max-w-md rounded-lg shadow-none transition-shadow duration-300 ease-in-out hover:shadow-lg hover:shadow-black/30"
+            />
+          </div>
+        </div>
+        <div className="max-w-6xl mx-auto space-y-4 my-5 flex flex-col md:flex-row justify-center items-center">
+          <TextComponent
+            title="Title about Us"
+            paragraph="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non consequatur voluptatem deserunt, delectus est deleniti. Illum, incidunt ab at accusamus corporis voluptates qui itaque reiciendis repudiandae doloremque sapiente ducimus iusto et ex magnam. Sed molestias quibusdam dolorum, quos vitae sequi autem perspiciatis aut ab obcaecati fuga eum tempore inventore maxime!"
+            className=""
+          />
+          <TextComponent
+            title="Title about Us"
+            paragraph="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non consequatur voluptatem deserunt, delectus est deleniti. Illum, incidunt ab at accusamus corporis voluptates qui itaque reiciendis repudiandae doloremque sapiente ducimus iusto et ex magnam. Sed molestias quibusdam dolorum, quos vitae sequi autem perspiciatis aut ab obcaecati fuga eum tempore inventore maxime!"
+            className=""
+          />
+        </div>
       </main>
     </Layout>
   )
