c5e8f93aa6b7ccac4da782d6dc8fa3acb985b2aa
James Karnes <karnes.james@gmail.com>
2023-04-15 16:17:53 -0400

    moved datepicker z-index

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -25,7 +25,7 @@ const ContactForm = () => {
             required
           />
           <label
-            for="name"
+            htmlFor="name"
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Full Name
@@ -41,7 +41,7 @@ const ContactForm = () => {
             required
           />
           <label
-            for="email"
+            htmlFor="email"
             className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Email address
@@ -55,7 +55,7 @@ const ContactForm = () => {
           >
             <label
               className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-              for="tourSelect"
+              htmlFor="tourSelect"
             >
               Choose a Tour:
             </label>
@@ -71,12 +71,12 @@ const ContactForm = () => {
             })}
           </select>
         </div>
-        <div class="relative z-0 mb-6 w-full group">
+        <div class="relative z-10 mb-6 w-full group">
           <DatePickerComponent />
         </div>
         <div class="relative z-0 mb-6 w-full group">
           <label
-            for="message"
+            htmlFor="message"
             className="block mb-2 text-sm font-medium text-gray-500"
           >
             Your message
