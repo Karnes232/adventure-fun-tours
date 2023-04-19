e86b9edbdf2566e4cdb749a145e4642249434a66
James Karnes <karnes.james@gmail.com>
2023-04-18 20:59:38 -0400

    testing font size for tour card

--- src/components/TourCardComponent/BackCard.js
+++ src/components/TourCardComponent/BackCard.js
@@ -5,12 +5,18 @@ const BackCard = ({ tour }) => {
   return (
     <div className="flip-card-back rounded-lg">
       <div className="text-start absolute top-9 left-3 text-primary-color space-y-6">
-        <p className="text-2xl">{name}</p>
+      <Link to={link} className=""><p className="text-2xl">{name}</p></Link>
         <p className="text-start text-lg">${price}</p>
         <p className="font-light pb-5">{description}</p>
         <Link to={link} className="underline text-xl underline-offset-8 z-30">
           More Info
         </Link>
+        <a
+          href="puntacana/tours/scuba"
+          target="_blank"
+          aria-label="Instagram"
+          rel="noreferrer"
+        >More Info</a>
       </div>
     </div>
   )
