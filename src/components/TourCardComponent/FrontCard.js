f985a6c2b66ca1aa8d6975750e1a098ac8769d51
James Karnes <karnes.james@gmail.com>
2023-04-21 10:55:54 -0400

    working on tour card

--- src/components/TourCardComponent/FrontCard.js
+++ src/components/TourCardComponent/FrontCard.js
@@ -1,7 +1,8 @@
+import { Link } from "gatsby"
 import React from "react"
 
 const FrontCard = ({ tour }) => {
-  const { name, img, price } = tour
+  const { name, img, price, description, link } = tour
   return (
     <div className="flip-card-front rounded-lg">
       <img
@@ -10,8 +11,12 @@ const FrontCard = ({ tour }) => {
         alt={name}
       />
       <div className="absolute bottom-3 left-3 text-primary-color">
-        <p className="text-lg">{name}</p>
-        <p className="text-start">${price}</p>
+        <p className="text-lg text-start">{name}</p>
+        <p className="text-start my-2">${price}</p>
+        <p className="text-start font-light pb-5">{description}</p>
+        <p className="text-start"><Link to={link} className="underline underline-offset-8">
+          More Info
+        </Link></p>
       </div>
     </div>
   )
