15423d14f154fc1da57c1269d1e3070489fafb3c
James Karnes <karnes.james@gmail.com>
2023-04-21 16:11:58 -0400

    working on tour card

--- src/components/TourCardComponent/TourCard.js
+++ src/components/TourCardComponent/TourCard.js
@@ -8,6 +8,7 @@ const TourCard = ({ tour }) => {
       <div className="flip-card w-[90vw] md:mx-auto md:w-80 h-96 my-2 md:my-6 rounded-lg">
         <div className="flip-card-inner rounded-lg">
           <FrontCard tour={tour} />
+          <BackCard tour={tour}/>
         </div>
       </div>
     </>
