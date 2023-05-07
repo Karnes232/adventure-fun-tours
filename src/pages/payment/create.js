0b37483a12fca1cd1d34818be3b7979a966c0246
James Karnes <karnes.james@gmail.com>
2023-05-07 14:03:59 -0400

    added paypal for deposits

--- src/pages/payment/create.js
+++ src/pages/payment/create.js
@@ -2,10 +2,9 @@ import React from "react"
 import InfoInputs from "../../components/PaymentComponents/InfoInputs"
 import Layout from "../../components/layout"
 import DatePickerComponent from "../../components/ContactForm/DatePickerComponent"
-import AmountInputs from '../../components/PaymentComponents/AmountInputs'
+import AmountInputs from "../../components/PaymentComponents/AmountInputs"
 
 const Create = () => {
- 
   return (
     <Layout>
       <main className="mt-28 md:mt-32 xl:mt-40">
@@ -15,23 +14,22 @@ const Create = () => {
           action="/payment/"
           id="payment"
           className="w-64 md:w-full max-w-md flex flex-col justify-center items-center mx-auto my-5"
-       
         >
           <>
             <InfoInputs />
           </>
           <>
-            <DatePickerComponent/>
+            <DatePickerComponent />
           </>
           <>
             <AmountInputs />
           </>
           <button
-          type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Submit
-        </button>
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
         </form>
       </main>
     </Layout>
--- src/pages/payment/index.js
+++ src/pages/payment/index.js
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from "react"
 import Layout from "../../components/layout"
+import CustomPayPal from "../../components/PayPalButtonWrapper/CustomPayPal"
 
 const Index = () => {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [location, setLocation] = useState('')
-  const [excursion, setExcursion] = useState('')
-  const [newDate, setNewDate] = useState('')
-  const [guests, setGuests] = useState('')
-  const [deposit, setDeposit] = useState('')
-  
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [location, setLocation] = useState("")
+  const [excursion, setExcursion] = useState("")
+  const [newDate, setNewDate] = useState("")
+  const [guests, setGuests] = useState("")
+  const [deposit, setDeposit] = useState("")
+  const [balance, setBalance] = useState("")
+
   useEffect(() => {
-    const options = {year: 'numeric', month: 'long', day: 'numeric' };
+    const options = { year: "numeric", month: "long", day: "numeric" }
     const searchParams = new URLSearchParams(document.location.search)
-    setName(searchParams.get('name'))
+    setName(searchParams.get("name"))
     setEmail(searchParams.get("email"))
     setLocation(searchParams.get("location"))
     setExcursion(searchParams.get("excursion"))
@@ -21,23 +23,47 @@ const Index = () => {
     setNewDate(new Date(date).toLocaleDateString("en-US", options))
     setGuests(searchParams.get("guests"))
     setDeposit(searchParams.get("deposit"))
+    setBalance(searchParams.get("balance"))
   }, [])
-  
- 
 
   return (
     <Layout>
-    <main className="mt-28 md:mt-32 xl:mt-40 flex flex-col justify-center items-center">
-    <div>
-      <div>Name: {name}</div>
-      <div>Email: {email}</div>
-      <div>Location: {location}</div>
-      <div>Excursion: {excursion}</div>
-      <div>Date: {newDate}</div>
-      <div>Amount of Guests: {guests}</div>
-      <div>Deposit Amount: ${deposit}</div>
-      </div>
+      <main className="mt-28 md:mt-32 xl:mt-40">
+        <div>
+          <div className="absolute top-0 w-full h-[100vh]">
+            <div className="min-h-screen min-w-full bg-center bg-no-repeat bg-cover paymentImage">
+              <div className="hero-text flex flex-col justify-center items-center text-primary-color w-11/12 md:w-9/12 lg:w-6/12 xl:w-4/12">
+                {name && <p className="text-2xl font-serif mb-2">{name}</p>}
+                {email && <p className="text-2xl font-serif mb-2">{email}</p>}
+                {location && (
+                  <p className="text-2xl font-serif mb-2">{location}</p>
+                )}
+                {excursion && (
+                  <p className="text-2xl font-serif mb-2">{excursion}</p>
+                )}
+                {newDate && (
+                  <p className="text-2xl font-serif mb-2">{newDate}</p>
+                )}
+                {guests && (
+                  <p className="text-2xl font-serif mb-2">Guests: {guests}</p>
+                )}
+                {deposit && (
+                  <p className="text-2xl font-serif mb-2">
+                    Deposit: ${deposit}
+                  </p>
+                )}
+                {balance && (
+                  <p className="text-2xl font-serif mb-8">
+                    Balance: ${balance}
+                  </p>
+                )}
+                <CustomPayPal price={deposit} />
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
+      <div className="pb-[86vh]"></div>
     </Layout>
   )
 }
