0f05b20f0ce752794717d9e62dee47b8d8b85e01
James Karnes <karnes.james@gmail.com>
2023-05-08 18:13:37 -0400

    added buggy to sosua tours list

--- src/data/tours/sosuaTours.js
+++ src/data/tours/sosuaTours.js
@@ -1,9 +1,11 @@
 import scuba from "./sosua/test/scuba"
 import horse from "./sosua/test/horse"
 import party from "./sosua/partyBoat"
+import buggy from './sosua/buggy'
 
 const sosuaTours = [
   party,
+  buggy,
   scuba,
   horse,
   scuba,
