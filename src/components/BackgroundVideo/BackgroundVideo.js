93306dc46d4e94e419a562ac70f0924ea5ee866b
James Karnes <karnes.james@gmail.com>
2023-05-12 14:22:53 -0400

    youtube test

--- src/components/BackgroundVideo/BackgroundVideo.js
+++ src/components/BackgroundVideo/BackgroundVideo.js
@@ -4,7 +4,10 @@ const BackgroundVideo = ({ video, className }) => {
   return (
     <div className={`bg-video-section bg-video-main ${className}`}>
       <div className="bg-video">
-        <video
+      <object data='https://www.youtube.com/embed/KvZT3etZIsw?autoplay=1' 
+        width='100%' height='100%'>
+    </object>
+        {/* <video
           className="bg-video__content"
           autoPlay
           muted
@@ -14,7 +17,7 @@ const BackgroundVideo = ({ video, className }) => {
         >
           <source src={video} type="video/mp4" />
           Your device does not support playing 'video/mp4' videos
-        </video>
+        </video> */}
       </div>
     </div>
   )
