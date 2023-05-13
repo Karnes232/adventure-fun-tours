423b17deb0616ccbe8136178a365b3cddc01f5e6
James Karnes <karnes.james@gmail.com>
2023-05-13 09:31:10 -0400

    edited BackgroundVideo.js to allow smaller videos for mobile to save bandwidth

--- src/components/BackgroundVideo/BackgroundVideo.js
+++ src/components/BackgroundVideo/BackgroundVideo.js
@@ -1,16 +1,21 @@
 import React from "react"
 
 const BackgroundVideo = ({ video, phoneVideo, className }) => {
+  const screenWidth = window.innerWidth
+  console.log(screenWidth)
   return (
     <div className={`bg-video-section bg-video-main ${className}`}>
       <div className="bg-video">
         <video className="bg-video__content" autoPlay muted loop playsInline>
-          <source
-            src={phoneVideo}
-            type="video/mp4"
-            media="all and (max-width: 480px)"
-          />
-          <source src={video} type="video/mp4" />
+        {/* Allows Smaller video for mobile devices to save bandwidth */}
+          {screenWidth < 600 ? (
+            <source
+              src={phoneVideo}
+              type="video/mp4"
+            />
+          ) : (
+            <source src={video} type="video/mp4" />
+          )}
           Your device does not support playing 'video/mp4' videos
         </video>
       </div>
