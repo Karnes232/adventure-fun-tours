9961001785572ce304296dfd8806cfd873a200e9
James Karnes <karnes.james@gmail.com>
2023-05-13 17:34:38 -0400

    added scubadoo information

--- src/data/tours/puntacanaTours.js
+++ src/data/tours/puntacanaTours.js
@@ -2,11 +2,13 @@ import catalinaDiving from "./puntacana/catalinaDiving"
 import catalinaSnorkel from "./puntacana/catalinaSnorkel"
 import partyBoat from "./puntacana/partyBoat"
 import divingCourses from "./puntacana/divingCourses"
+import scubaDoo from "./puntacana/scubaDoo"
 const puntacanaTours = [
   catalinaDiving,
   catalinaSnorkel,
   partyBoat,
   divingCourses,
+  scubaDoo
 ]
 
 export default puntacanaTours
