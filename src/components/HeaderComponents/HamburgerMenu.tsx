91519b3e9ec8b172561fdb30144603226af5b412
James Karnes <karnes.james@gmail.com>
2023-05-14 19:07:05 -0400

    logo test

--- src/components/HeaderComponents/HamburgerMenu.tsx
+++ src/components/HeaderComponents/HamburgerMenu.tsx
@@ -13,11 +13,11 @@ const HamburgerMenu = () => {
           <>
             <span className="rounded-md shadow-sm">
               <Menu.Button
-                className="inline-flex justify-center w-10 px-4 py-2 font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-transparent border border-[#F14E37] rounded-md hover:text-gray-500 focus:outline-none focus:border-gray-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"
+                className="inline-flex justify-center w-10 px-4 py-2 font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-transparent border border-[#F58634] rounded-md hover:text-gray-500 focus:outline-none focus:border-gray-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"
                 aria-label="menu"
               >
                 <span>
-                  <Bars3Icon className="h-6 text-[#F14E37]" />
+                  <Bars3Icon className="h-6 text-[#F58634]" />
                 </span>
                 <svg
                   className="w-5 h-5"
