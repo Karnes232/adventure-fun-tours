3a5838fb0523b005a5056f24b632759b874c4dae
James Karnes <karnes.james@gmail.com>
2023-05-14 17:25:55 -0400

    testing white logo

--- src/components/HeaderComponents/Logo.tsx
+++ src/components/HeaderComponents/Logo.tsx
@@ -9,8 +9,15 @@ const Logo = () => {
       <div className="flex justify-center items-center">
         <Link to="/" className="no-underline" aria-label="Home">
           <div className="flex p-2 md:p-6 items-center w-36 h-28 cursor-pointer md:w-48 md:h-32 xl:w-64 xl:h-40">
-            <StaticImage
+            {/* <StaticImage
               src="../../images/logo-fun.png"
+              loading="eager"
+              quality={100}
+              formats={["auto", "webp", "avif"]}
+              alt=""
+            /> */}
+            <StaticImage
+              src="../../images/logo-white.png"
               loading="eager"
               quality={100}
               formats={["auto", "webp", "avif"]}
