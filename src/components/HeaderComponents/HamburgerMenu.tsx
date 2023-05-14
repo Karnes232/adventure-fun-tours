bb7dd8942ff24a5b06c31b52fe9da2dc53600ac4
James Karnes <karnes.james@gmail.com>
2023-05-14 19:29:48 -0400

    logo testing

--- src/components/HeaderComponents/HamburgerMenu.tsx
+++ src/components/HeaderComponents/HamburgerMenu.tsx
@@ -13,7 +13,7 @@ const HamburgerMenu = () => {
           <>
             <span className="rounded-md shadow-sm">
               <Menu.Button
-                className="inline-flex justify-center w-10 px-4 py-2 font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-transparent border border-[#F58634] rounded-md hover:text-gray-500 focus:outline-none focus:border-gray-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"
+                className="inline-flex justify-center w-10 px-4 py-2 font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-transparent border border-[#F58634] rounded-md hover:text-gray-500 focus:outline-none focus:border-[#F58634] focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"
                 aria-label="menu"
               >
                 <span>
