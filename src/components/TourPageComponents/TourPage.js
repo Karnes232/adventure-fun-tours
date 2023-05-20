c39ed3ef3c6c3a374252819df8690fe685aff350
James Karnes <karnes.james@gmail.com>
2023-05-20 16:42:48 -0400

    added conditional rendering for tour page

--- src/components/TourPageComponents/TourPage.js
+++ src/components/TourPageComponents/TourPage.js
@@ -13,79 +13,82 @@ import Button from "./Button"
 const TourPage = ({ tour }) => {
   return (
     <>
-    {tour.name && <Helmet title={tour.name} />}
-      
       {tour && (
-        <main>
-          {tour.img && <HeroComponent image={tour.img} />}
-          <div className="max-w-6xl my-5 mx-5 md:mx-10 xl:mx-auto">
-            {tour.name && (
-              <div className="max-w-6xl my-2 xl:mx-auto">
-                <h4 className="font-light text-3xl md:text-4xl tracking-wide">
-                  {tour.name}
-                </h4>
-                <div className="flex flex-col md:flex-row md:justify-between">
-                  <p className="font-extralight my-2 text-base md:text-lg tracking-wide">
-                    {tour.location}
-                  </p>
-                  <Button text="Check Availability" customClass="" />
+        <>
+          {tour.name && <Helmet title={tour.name} />}
+          <main>
+            {tour.img && <HeroComponent image={tour.img} />}
+            <div className="max-w-6xl my-5 mx-5 md:mx-10 xl:mx-auto">
+              {tour.name && (
+                <div className="max-w-6xl my-2 xl:mx-auto">
+                  <h4 className="font-light text-3xl md:text-4xl tracking-wide">
+                    {tour.name}
+                  </h4>
+                  <div className="flex flex-col md:flex-row md:justify-between">
+                    <p className="font-extralight my-2 text-base md:text-lg tracking-wide">
+                      {tour.location}
+                    </p>
+                    <Button text="Check Availability" customClass="" />
+                  </div>
                 </div>
-              </div>
-            )}
-            {tour.available && (
-              <AvailableComponent
-                available={tour.available}
-                price={tour.price}
-              />
-            )}
-            {tour.tourPage.title1 && (
-              <div className="my-5">
-                <TextComponent
-                  title={tour.tourPage.title1}
-                  paragraph={tour.tourPage.paragraph1}
-                />{" "}
-              </div>
-            )}
-          </div>
-          {tour.photos && (
-            <div className="my-5 max-w-6xl lg:mx-auto">
-              <SwiperCarousel tour={tour} />
-            </div>
-          )}
-
-          <div className="max-w-6xl my-5 mx-5 md:mx-10 xl:mx-auto">
-            {tour.tourPage.title2 && (
-              <div className="my-5">
-                <TextComponent
-                  title={tour.tourPage.title2}
-                  paragraph={tour.tourPage.paragraph2}
+              )}
+              {tour.available && (
+                <AvailableComponent
+                  available={tour.available}
+                  price={tour.price}
                 />
+              )}
+              {tour.tourPage.title1 && (
+                <div className="my-5">
+                  <TextComponent
+                    title={tour.tourPage.title1}
+                    paragraph={tour.tourPage.paragraph1}
+                  />{" "}
+                </div>
+              )}
+            </div>
+            {tour.photos && (
+              <div className="my-5 max-w-6xl lg:mx-auto">
+                <SwiperCarousel tour={tour} />
               </div>
             )}
-            {tour.tourPage.title3 && (
-              <TextComponent
-                title={tour.tourPage.title3}
-                paragraph={tour.tourPage.paragraph3}
+
+            <div className="max-w-6xl my-5 mx-5 md:mx-10 xl:mx-auto">
+              {tour.tourPage.title2 && (
+                <div className="my-5">
+                  <TextComponent
+                    title={tour.tourPage.title2}
+                    paragraph={tour.tourPage.paragraph2}
+                  />
+                </div>
+              )}
+              {tour.tourPage.title3 && (
+                <TextComponent
+                  title={tour.tourPage.title3}
+                  paragraph={tour.tourPage.paragraph3}
+                />
+              )}
+              <div className="flex flex-col lg:flex-row lg:space-x-8 lg:justify-center">
+                {tour.included && (
+                  <IncludedComponent included={tour.included} />
+                )}
+                {tour.goodInfo && <GoodInfo goodInfo={tour.goodInfo} />}
+                {tour.extras && <ExtraInfo extras={tour.extras} />}{" "}
+              </div>
+              <div className="mt-2">
+                <Button text="Book Now" customClass="lg:px-20 lg:py-4" />
+              </div>
+            </div>
+
+            {tour.video && (
+              <BackgroundVideo
+                video={tour.video}
+                phoneVideo={tour.videoPhone}
+                className=""
               />
             )}
-            <div className="flex flex-col lg:flex-row lg:space-x-8 lg:justify-center">
-              {tour.included && <IncludedComponent included={tour.included} />}
-              {tour.goodInfo && <GoodInfo goodInfo={tour.goodInfo} />}
-              {tour.extras && <ExtraInfo extras={tour.extras} />}{" "}
-            </div>
-            <div className="mt-2">
-              <Button text="Book Now" customClass="lg:px-20 lg:py-4" />
-            </div>
-          </div>
-
-          {tour.video && (
-            <BackgroundVideo
-              video={tour.video}
-              phoneVideo={tour.videoPhone}
-              className=""
-            />
-          )}
-        </main>
+          </main>
+        </>
       )}
     </>
   )
--- src/data/tours/puntacana/zipLine.js
+++ src/data/tours/puntacana/zipLine.js
@@ -18,14 +18,18 @@ const zipLine = {
   img: mainPhoto,
   price: "89",
   cost: "50",
-  description: "Get ready for an adrenaline-fueled aerial experience with our thrilling zipline tour in Punta Cana.",
+  description:
+    "Get ready for an adrenaline-fueled aerial experience with our thrilling zipline tour in Punta Cana.",
   tourPage: {
     title1: "Experience the Ultimate Thrill",
-    paragraph1: "Embark on an exhilarating journey through the treetops, soaring above lush landscapes and enjoying breathtaking views of the Dominican Republic's natural beauty. Strap in, let go of your fears, and embrace the excitement of this unforgettable zipline adventure. Our zipline tour offers an unparalleled thrill as you soar through the skies, feeling the rush of adrenaline with each breathtaking descent. Traverse a series of sturdy cables, securely attached to a harness, and witness the world from a unique perspective. Let the wind rush through your hair and the freedom of flight fill your senses as you conquer gravity and experience the ultimate thrill of ziplining.",
+    paragraph1:
+      "Embark on an exhilarating journey through the treetops, soaring above lush landscapes and enjoying breathtaking views of the Dominican Republic's natural beauty. Strap in, let go of your fears, and embrace the excitement of this unforgettable zipline adventure. Our zipline tour offers an unparalleled thrill as you soar through the skies, feeling the rush of adrenaline with each breathtaking descent. Traverse a series of sturdy cables, securely attached to a harness, and witness the world from a unique perspective. Let the wind rush through your hair and the freedom of flight fill your senses as you conquer gravity and experience the ultimate thrill of ziplining.",
     title2: "Immerse Yourself in Nature's Beauty",
-    paragraph2: "As you glide through the air, take in the awe-inspiring views of Punta Cana's natural landscapes. Admire the lush greenery, observe the diverse flora and fauna, and appreciate the pristine beauty of the surroundings. Our zipline tour offers not only an adrenaline rush but also an opportunity to connect with nature and immerse yourself in its captivating allure. Our zipline adventure is designed to cater to adventurers of all ages and experience levels. Whether you're a seasoned zipliner or a first-time flyer, our professional guides will ensure your safety and provide expert guidance throughout the tour. So gather your friends, family, or fellow thrill-seekers and get ready to create lasting memories together.",
+    paragraph2:
+      "As you glide through the air, take in the awe-inspiring views of Punta Cana's natural landscapes. Admire the lush greenery, observe the diverse flora and fauna, and appreciate the pristine beauty of the surroundings. Our zipline tour offers not only an adrenaline rush but also an opportunity to connect with nature and immerse yourself in its captivating allure. Our zipline adventure is designed to cater to adventurers of all ages and experience levels. Whether you're a seasoned zipliner or a first-time flyer, our professional guides will ensure your safety and provide expert guidance throughout the tour. So gather your friends, family, or fellow thrill-seekers and get ready to create lasting memories together.",
     title3: "Unforgettable Memories",
-    paragraph3: "Each zipline ride is a chance to create lasting memories. Capture stunning photos of yourself in mid-air, surrounded by the breathtaking landscapes of Punta Cana. Share laughter, excitement, and camaraderie with your fellow adventurers as you conquer each zipline course together. The memories you make during this exhilarating experience will be cherished for a lifetime. Ready to take flight and experience the thrill of ziplining in Punta Cana? Contact our team directly to inquire about availability, pricing, and any additional information you may need for the zipline tour in partnership with Bavaro Adventure Park. Our dedicated staff will assist you in planning your excursion and ensuring a seamless and unforgettable adventure.",
+    paragraph3:
+      "Each zipline ride is a chance to create lasting memories. Capture stunning photos of yourself in mid-air, surrounded by the breathtaking landscapes of Punta Cana. Share laughter, excitement, and camaraderie with your fellow adventurers as you conquer each zipline course together. The memories you make during this exhilarating experience will be cherished for a lifetime. Ready to take flight and experience the thrill of ziplining in Punta Cana? Contact our team directly to inquire about availability, pricing, and any additional information you may need for the zipline tour in partnership with Bavaro Adventure Park. Our dedicated staff will assist you in planning your excursion and ensuring a seamless and unforgettable adventure.",
   },
   available: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
   included: ["transportation", "Safety Equipment", "Lunch"],
@@ -34,7 +38,7 @@ const zipLine = {
     "Bring Digital or a printed voucher",
     "All guests must be mobile and in good physical condition",
     "Transportation may not be available from some resorts or may be subject to an additional fee",
-    "Cash or credit card for souvenirs, pictures, snacks & drinks, tips, lockers"
+    "Cash or credit card for souvenirs, pictures, snacks & drinks, tips, lockers",
   ],
   extras: {
     duration: "8 hours (approx.)",
