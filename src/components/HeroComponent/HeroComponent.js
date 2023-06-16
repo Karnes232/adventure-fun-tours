83f5821a6bffa3537f9eb6a9a23fe63d62864234
James Karnes <karnes.james@gmail.com>
2023-06-16 17:09:43 -0400

    adjust width of h1 text on hero component

--- src/components/HeroComponent/HeroComponent.js
+++ src/components/HeroComponent/HeroComponent.js
@@ -4,7 +4,7 @@ const HeroComponent = ({ page, h1, p }) => {
   return (
     <div className="absolute top-0 w-full h-[75vh] lg:h-[80vh]">
       <div className={`hero bgImage ${page}`}>
-        <div className="hero-text flex flex-col justify-center items-center text-primary-color w-11/12 md:w-9/12 lg:w-6/12 xl:w-4/12">
+        <div className="hero-text flex flex-col justify-center items-center text-primary-color w-11/12 md:w-9/12 lg:w-[37rem]">
           {h1 && (
             <h1 className="text-4xl md:text-6xl font-bold mb-6 font-serif">
               {h1}
