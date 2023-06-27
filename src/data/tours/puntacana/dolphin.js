54402cbeffa34a2b3b302e81ad73b2f8d4b1a447
James Karnes <karnes.james@gmail.com>
2023-06-27 19:17:21 -0400

    updated dolphin information

--- src/data/tours/puntacana/dolphin.js
+++ src/data/tours/puntacana/dolphin.js
@@ -26,20 +26,31 @@ const dolphin = {
   location: "Punta Cana",
   link: "/puntacana/tours/dolphin/",
   img: mainPhoto,
-  price: "185",
-  cost: "129",
+  price: "120",
+  cost: "100",
   description:
     "Dive into an extraordinary adventure that will create lasting memories and a deep connection with these magnificent creatures",
+  // tourPage: {
+  //   title1: "Swim with Dolphins: Experience the Magic of the Sea",
+  //   paragraph1:
+  //     "Immerse yourself in a once-in-a-lifetime encounter with dolphins and create unforgettable memories with our incredible swim with dolphins tour. Our partnership with Dolphin Explorer allows us to offer you this extraordinary opportunity to interact and swim alongside these magnificent creatures in their natural habitat. Our swim with dolphins tour is designed to provide a safe and educational experience while allowing you to connect with these intelligent marine mammals on a deeper level. Prepare to be amazed as you witness their grace, playfulness, and natural behaviors up close.",
+  //   title2: "Swim, Interact, and Learn",
+  //   paragraph2:
+  //     "During the tour, you'll have the opportunity to swim and interact with the dolphins under the guidance of experienced trainers. Learn about their behavior, communication, and unique characteristics while forming a special bond with these gentle creatures. From gentle caresses to thrilling rides alongside them, every moment will leave you in awe. Swimming with dolphins is not only a thrilling adventure but also an enriching experience that fosters a deeper understanding and appreciation for these incredible creatures and their natural habitat. Our tour aims to promote conservation and respect for marine life, inspiring guests to become advocates for the protection of our oceans.",
+  //   title3: "Book Your Swim with Dolphins Tour",
+  //   paragraph3:
+  //     "Ready to embark on an unforgettable journey with dolphins? Contact us directly to inquire about availability, pricing, and any additional information you may need for the swim with dolphins tour. Our team will ensure a seamless booking process and provide you with all the necessary details to make your experience truly remarkable. Please note that swim with dolphins reservations are exclusively handled through our team. To secure your spot or learn more about the tour, kindly reach out to us via email or through our contact page. We are thrilled to assist you in creating an extraordinary swim with dolphins adventure.",
+  // },
   tourPage: {
-    title1: "Swim with Dolphins: Experience the Magic of the Sea",
+    title1: "Dolphin Family Experience",
     paragraph1:
-      "Immerse yourself in a once-in-a-lifetime encounter with dolphins and create unforgettable memories with our incredible swim with dolphins tour. Our partnership with Dolphin Explorer allows us to offer you this extraordinary opportunity to interact and swim alongside these magnificent creatures in their natural habitat. Our swim with dolphins tour is designed to provide a safe and educational experience while allowing you to connect with these intelligent marine mammals on a deeper level. Prepare to be amazed as you witness their grace, playfulness, and natural behaviors up close.",
-    title2: "Swim, Interact, and Learn",
+      "Our program for those who come with children and for those who want to swim with dolphins even if they have no previous swimming experience. Our dolphins will receive you on our platform and you will enjoy an unforgettable experience. Starting at $120 for a 30 minute experience.",
+    title2: "Dolphin Action Program",
     paragraph2:
-      "During the tour, you'll have the opportunity to swim and interact with the dolphins under the guidance of experienced trainers. Learn about their behavior, communication, and unique characteristics while forming a special bond with these gentle creatures. From gentle caresses to thrilling rides alongside them, every moment will leave you in awe. Swimming with dolphins is not only a thrilling adventure but also an enriching experience that fosters a deeper understanding and appreciation for these incredible creatures and their natural habitat. Our tour aims to promote conservation and respect for marine life, inspiring guests to become advocates for the protection of our oceans.",
-    title3: "Book Your Swim with Dolphins Tour",
+      "For those who want to have a first-hand experience with our dolphins. You will meet them on our platform, where they will greet you and enjoy their acrobatics, applause and courtesies, before giving you an emotional tow of pectoral fins in the sea! Starting at $155 for a 40 minute experience.",
+    title3: "Dolphin Royal Swim",
     paragraph3:
-      "Ready to embark on an unforgettable journey with dolphins? Contact us directly to inquire about availability, pricing, and any additional information you may need for the swim with dolphins tour. Our team will ensure a seamless booking process and provide you with all the necessary details to make your experience truly remarkable. Please note that swim with dolphins reservations are exclusively handled through our team. To secure your spot or learn more about the tour, kindly reach out to us via email or through our contact page. We are thrilled to assist you in creating an extraordinary swim with dolphins adventure.",
+      "Our dolphins will greet you with a formal handshake and you will also enjoy their acrobatics. Get on the ride of your life and enjoy a dorsal fin drag or the incredible foot push. Two dolphins will lift you across the surface of the water by pushing your feet. Starting at $195 for a 50 minute experience.",
   },
   available: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
   included: ["Transportation", "Swim with Dolphin"],
